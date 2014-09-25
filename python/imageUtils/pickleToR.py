7de8de36bf75de2a535c4ad5e09967d4d96e68ca
Oliver4242 <oliver.duerr@gmail.com>
2014-09-25 09:29:01 +0200

    Testing wlan

--- python/imageUtils/pickleToR.py
+++ python/imageUtils/pickleToR.py
@@ -1,10 +1,10 @@
 import pickle
 import numpy as np
 
-(X_train, y_train) = pickle.load(open("/Users/oli/Proj_Large_Data/PiVision/pivision/trunk/python/pickeledStuff/training_48x48_aligned.p", "r"))
+(X_train, y_train) = pickle.load(open("/Users/oli/Proj_Large_Data/PiVision/pivision/trunk/python/pickeledStuff/training_48x48_aligned_large.p", "r"))
 print("Loaded " + str(len(y_train)) + " training data")
 import csv
-filename = "training_aligned_R.txt"
+filename = "/Users/oli/Proj_Large_Data/PiVision/pivision/trunk/python/pickeledStuff/training_48x48_aligned_large.p_R"
 w = csv.writer(open(filename + '.csv', 'w'))
 for c in range(0, len(X_train)):
     d = np.append(y_train[c], X_train[c].reshape(-1))
