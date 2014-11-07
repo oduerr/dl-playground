9515f7943a48068dc6d8ce2cb3d5922465de8168
Oliver4242 <oliver.duerr@gmail.com>
2014-11-07 18:18:48 +0100

    Added Gini-Index predictions with Gini < 0.8 are done

--- python/FaceRec/LeNetPredictor.py
+++ python/FaceRec/LeNetPredictor.py
@@ -92,6 +92,42 @@ class LeNetPredictor(object):
         return (self.predict_model(values))
 
 
+def GRLC(values):
+    '''
+    Calculate Gini index, Gini coefficient, Robin Hood index, and points of
+    Lorenz curve based on the instructions given in
+    www.peterrosenmai.com/lorenz-curve-graphing-tool-and-gini-coefficient-calculator
+    Lorenz curve values as given as lists of x & y points [[x1, x2], [y1, y2]]
+    @param values: List of values
+    @return: [Gini index, Gini coefficient, Robin Hood index, [Lorenz curve]]
+    '''
+    n = len(values)
+    assert(n > 0), 'Empty list of values'
+    sortedValues = sorted(values) #Sort smallest to largest
+
+    #Find cumulative totals
+    cumm = [0]
+    for i in range(n):
+        cumm.append(sum(sortedValues[0:(i + 1)]))
+
+    #Calculate Lorenz points
+    LorenzPoints = [[], []]
+    sumYs = 0           #Some of all y values
+    robinHoodIdx = -1   #Robin Hood index max(x_i, y_i)
+    for i in range(1, n + 2):
+        x = 100.0 * (i - 1)/n
+        y = 100.0 * (cumm[i - 1]/float(cumm[n]))
+        LorenzPoints[0].append(x)
+        LorenzPoints[1].append(y)
+        sumYs += y
+        maxX_Y = x - y
+        if maxX_Y > robinHoodIdx: robinHoodIdx = maxX_Y
+
+    giniIdx = 100 + (100 - 2 * sumYs)/n #Gini index
+
+    return [giniIdx, giniIdx/100, robinHoodIdx, LorenzPoints]
+
+
 if __name__ == "__main__":
     import os
     import Utils_dueo
@@ -102,11 +138,12 @@ if __name__ == "__main__":
         stateIn = None
     pred = LeNetPredictor(stateIn=stateIn)
     print("Loaded Predictor ")
-    filenameTesting    = "../../data/testing_48x48_unaligned_large.p_R.csv.gz"
+    filenameTesting    = "../../data/testing_48x48_aligned_large.p_R.csv.gz"
     totTime = 0
     count = 0
     show = True
     ok = 0
+    preds = 0
     import matplotlib.pyplot as plt
     plt.ion()
     pos = np.arange(6)+.5
@@ -122,20 +159,26 @@ if __name__ == "__main__":
             predPerson = int(res.argmax())
             plt.clf()
             plt.yticks(pos, ('Dejan', 'Diego', 'Martin', 'Oliver', 'Rebekka', 'Ruedi'))
+            gini = GRLC(res[0])[0]
+            if (gini > 80):
+                preds += 1
             if predPerson == truePerson:
-                ok += 1
+                if gini > 80:
+                    ok += 1
                 col = 'g'
             else:
                 col = 'r'
             plt.barh(pos, np.asarray(res[0], dtype = float), align='center', color = col)
             plt.draw()
-            time.sleep(0.5)
+            plt.title("Gini Index" + str(gini) + " allOK " + str(ok))
+            time.sleep(0.01)
             if predPerson != truePerson:
-                time.sleep(1)
-            print(str(truePerson) + " " + str(predPerson) + str(res))
+                time.sleep(0.01)
+            print(str(truePerson) + " " + str(predPerson) + str(gini))
             count += 1
     plt.show()
-    print("Total Time " + str(totTime) + "sec for " + str(count) + " faces. Accuracy " + str((1. * ok) / count))
+    print("Total Time " + str(totTime) + "sec for " + str(count) + " faces. Accuracy " + str((1. * ok) / preds)
+          + " predicted " + str(preds))
 
 
 
