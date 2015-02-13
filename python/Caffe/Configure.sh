556a81cd81a9eca9e106e5de3a21b0c16cb9bd81
Oliver4242 <oliver.duerr@gmail.com>
2015-02-13 18:16:10 +0100

    Configuration file loading library and the like

---
+++ python/Caffe/Configure.sh
@@ -0,0 +1,4 @@
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+PYTHONPATH="${PYTHONPATH}:/home/dueo/caffe/caffe/python";export PYTHONPATH;printenv PYTHONPATH
+PATH="${PATH}:/home/dueo/caffe/caffe/python";export PATH;printenv PATH
+echo "Set path for caffe"
