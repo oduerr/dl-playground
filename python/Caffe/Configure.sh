d2612f931990846ce744129326e2d4a67f6057b3
Oliver4242 <oliver.duerr@gmail.com>
2015-02-14 17:59:41 +0100

    Remove duplicated directory

--- python/Caffe/Configure.sh
+++
@@ -1,8 +0,0 @@
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-PYTHONPATH="${PYTHONPATH}:/home/dueo/caffe/caffe/python"
-export PYTHONPATH
-printenv PYTHONPATH
-PATH="${PATH}:/home/dueo/caffe/caffe/python"
-export PATH
-printenv PATH
-echo "Set path for caffe"
