9f07b6b8eec2b60517f127bac392da56d6f5ddc9
Oliver4242 <oliver.duerr@gmail.com>
2015-02-14 17:45:10 +0100

    Put some image files local

--- python/Caffe/TestCNN.py
+++ python/Caffe/TestCNN.py
@@ -7,8 +7,21 @@ import caffe
 print("Finshed imports")
 
 if __name__ == "__main__":
-  print("Hallo Gallo")
+  print("#############################################################")
+  print("Training the model")
+  print("#############################################################")
   caffe.set_mode_cpu()
   caffe.set_phase_train()
-  #caffe.SGDSolver('lenet_train_test_files.prototxt')
-  caffe.SGDSolver('')
+  res = caffe.SGDSolver('lenet_solver.prototxt')
+  print("#############################################################")
+  print("Finished Training the model")
+  print("#############################################################")
+  res.net.save('model/modelT')
+  # Loading the classifier (no direct way?)
+  net = caffe.Classifier('lenet_deploy.prototxt', 'model/modelT', image_dims=(46, 46))
+  input_image = caffe.io.load_image('/home/dueo/data_kaggel_bowl/train_resized/hydromedusae_h15/138113.jpg')
+  prediction = net.predict([input_image]) 
+  print("The pridiction is")
+  print(prediction)
+ 
+    
