dcbff0e5e769089d64fe530f68a900a9aee718c8
Oliver4242 <oliver.duerr@gmail.com>
2015-02-25 22:39:39 +0100

    Fixed a quite stupid bug (only using the 1 instead of the i's) entry of a blob.

---
+++ python/FaceCaffe/loadingModel.py
@@ -0,0 +1,33 @@
+__author__ = 'oli'
+import sys
+import csv,os
+import numpy as np
+import time
+sys.path.append('~/caffe/caffe/python/caffe')
+caffe_root = '/home/dueo/caffe/caffe/' 
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+print("Finshed imports")
+
+
+if __name__ == "__main__":
+  caffe.set_mode_cpu()
+  caffe.set_phase_train
+  net = caffe.Net('lenet_train_test_files.prototxt', 'snapshots/lenet25Feb_iter_12000.caffemodel')
+  start = time.time()
+  res = net.forward() # this will load the next mini-batch as defined in the net (rewinds)
+  logloss = res['loss'][0][0][0][0]
+  print(res)
+  print ("Time for a single forward step " + str((time.time() - start)))
+  preds = net.blobs['ip2'].data 
+  batchSize = np.shape(preds)[0]
+  yTrues = np.reshape(net.blobs['label'].data, batchSize).astype(int)
+  summe = 0
+  acc = 0
+  for i,yTrue in enumerate(yTrues):
+    pred = np.reshape(preds[i], 6)
+    prob = np.exp(pred)/np.sum(np.exp(pred))
+    print(str(i) + " " + str(prob[yTrue]) + " yTrue " + str(yTrue) + " pred " + str(np.argmax(prob)))
+    summe -= np.log(prob[yTrue])
+    acc += (np.argmax(prob) == yTrue)
+  print("Calculated  logloss()" + str(summe/batchSize) + "  acc=" + str(acc / batchSize) + " logloss(caffe layer)=" + str(logloss))
