b9b9d700b245705adbf5c729a37bde50c77d777a
Oliver4242 <oliver.duerr@gmail.com>
2015-02-27 21:42:29 +0100

    Code for Forum

--- python/PlanktonCaffe/createSubmission2.py
+++ python/PlanktonCaffe/createSubmission2.py
@@ -1,4 +1,3 @@
-__author__ = 'oli'
 import sys
 import csv,os
 import numpy as np
@@ -7,42 +6,51 @@ sys.path.append('~/caffe/caffe/python/caffe')
 caffe_root = '/home/dueo/caffe/caffe/' 
 sys.path.insert(0, caffe_root + 'python')
 import caffe
-print("Finshed imports")
 
-# For google net
-#predLayerName = 'loss3/classifier' #For the google-net
-#logLossName = 'loss3/loss3' 
-#description = 'googlenet/train_val.prototxt'
-#learnedModel = 'googlenet/models/googlenet_quick_iter_280000.caffemodel'
-
-predLayerName = 'loss3/classifier' #For the google-net
-logLossName = 'ip2' 
-description = 'lenet/lenet_train_test_files.prototxt'
-learnedModel = 'lenet/model/lenet60_iter_100000.caffemodel '
-
+predLayerName = 'fc8' #Output of the last inner product layer
+description = 'val_kaggle.prototxt'
+learnedModel = 'models/alexnet_train_iter_5000.caffemodel'
+sampleSub    ='../sampleSubmission.csv.head.csv' #We just need the header
+submissionName = 'submission.txt'
 
 if __name__ == "__main__":
+  # Getting the names of the validation set
+  names = []
+  with open('../test_kaggle_full.txt', 'rb') as f:
+    reader = csv.reader(f, delimiter=' ')
+    for row in reader:
+        names.append(row[0].split("//")[1]) #This file contains 130'000 lines like: /home/dueo/data_kaggel_bowl/test//1.jpg 42
+  
+  # Getting the header
+  fc = csv.reader(file(sampleSub))
+  fst =  fc.next()
+  # Opening the submission file
+  fout = open(submissionName, 'w');
+  w = csv.writer(fout);
+  w.writerow(fst)
+  
   caffe.set_mode_gpu()
   caffe.set_phase_test()
-  # Taken from:https://github.com/BVLC/caffe/issues/1774
+  # Taken from: https://github.com/BVLC/caffe/issues/1774
   net = caffe.Net(description, learnedModel)
-  start = time.time()
   read = 0
-  while(True):
+  while(read < len(names)):
+    start = time.time()
     res = net.forward() # this will load the next mini-batch as defined in the net (rewinds)
-    logloss = res[logLossName][0][0][0][0]
-    print(res)
-    print ("Time for a single forward step " + str((time.time() - start)))
+    print ("Time for getting the batch " + str((time.time() - start)) + " " + str(read))
     preds = net.blobs[predLayerName].data 
     batchSize = np.shape(preds)[0]
-    yTrues = np.reshape(net.blobs['label'].data, batchSize).astype(int)
-    summe = 0.0
-    acc = 0.0
     for i in range(0,batchSize):
-      yTrue = yTrues[i]
-      pred = np.reshape(preds[i],121)
+      pred = np.reshape(preds[i],1000)[0:121] #Todo change to 121
+      #pred = np.reshape(preds[i],121)
       prob = np.exp(pred)/np.sum(np.exp(pred))
-      #print(str(i) + " " + str(prob[yTrue]) + " yTrue " + str(yTrue) + " pred " + str(np.argmax(prob)))
-      summe -= np.log(prob[yTrue])
-      acc += (np.argmax(prob) == yTrue)
-    print("Hallo   logloss()" + str(summe/batchSize) + "  acc=" + str(acc / batchSize) + " logloss(caffe)=" + str(logloss))
+      s = names[read]
+      for r in prob:
+          s = s + ',' + "{0:.4}".format(r)
+      fout.write(s + '\n')
+      read += 1
+      if (read >= len(names)):
+        break
+  
+    
+      
