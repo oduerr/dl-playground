c40f6804221cf681309af5091f466f91f4320241
Oliver4242 <oliver.duerr@gmail.com>
2015-02-27 16:06:14 +0100

    rotated image

--- python/PlanktonCaffe/imagenet/make_imagenet_mean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# Compute the mean image from the imagenet training leveldb
-# N.B. this is available in data/ilsvrc12
-
-/home/dueo/caffe/caffe/build/tools/compute_image_mean /home/dueo/data_kaggel_bowl/train_lmdb_64 \
-  /home/dueo/data_kaggel_bowl/train_mean_64.binaryproto
-
-echo "Done."
