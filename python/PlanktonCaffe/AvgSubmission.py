6b5c8d29239b91bdcf3190e401fb47a71d3e950f
Oliver4242 <oliver.duerr@gmail.com>
2015-03-10 23:46:17 +0100

    Checking Submission

---
+++ python/PlanktonCaffe/AvgSubmission.py
@@ -0,0 +1,31 @@
+__author__ = 'oli'
+
+import csv
+
+fc1 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big6_15000.txt'))
+fc2 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big6_21000.txt'))
+fc3 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big6_36000.txt'))
+fc4 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big8_102k.txt'))
+
+fout = open('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_6_8_join.txt', 'w')
+w = csv.writer(fout)
+
+fst =  fc1.next();fc2.next();fc3.next();fc4.next();
+w.writerow(fst)
+
+
+for row1 in fc1:
+    row2 = fc2.next()
+    row3 = fc3.next()
+    row4 = fc4.next()
+    newLine = [row1[0]]
+    for c in range(1, len(row1)):
+        newLine.append(str((float(row1[c]) + float(row2[c]) + float(row3[c]) + float(row4[c]))/4.0))
+    w.writerow(newLine);
+fout.close()
+
+
+
+
+
+
