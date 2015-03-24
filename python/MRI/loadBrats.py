55d8506f1329f1c899f14a0712c6c90eea1bf56d
Oliver4242 <oliver.duerr@gmail.com>
2015-03-24 20:20:51 +0100

    Unstaged file before cleaning

--- python/MRI/loadBrats.py
+++ python/MRI/loadBrats.py
@@ -3,7 +3,9 @@ import sys
 import mha
 
 file1 = '/home/dueo/data/BRATS/BRATS-2/Image_Data/HG/0001/VSD.Brain.XX.O.MR_T1/VSD.Brain.XX.O.MR_T1.685.mha'
-img=mha.new(input_file=file1, data_type='long')
+file1 = '/Users/oli/Proj_Large_Data/Deep_Learning_MRI/BRATS-2/Image_Data/HG/0001/VSD.Brain.XX.O.MR_T1/VSD.Brain.XX.O.MR_T1.685.mha'
+
+img=mha.new(input_file=file1)
 img.read_mha(file1)
 
 #from medpy.io import load
--- python/PlanktonCaffe/AvgSubmission.py
+++ python/PlanktonCaffe/AvgSubmission.py
@@ -6,11 +6,12 @@ fc1 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big
 fc2 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big6_21000.txt'))
 fc3 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big6_36000.txt'))
 fc4 = csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big8_102k.txt'))
+fc5= csv.reader(file('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_big8_102k_test_10_aug.txt'))
 
-fout = open('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_6_8_join.txt', 'w')
+fout = open('/Users/oli/Proj_Large_Data/kaggle_plankton/submission_6_8aug__join.txt', 'w')
 w = csv.writer(fout)
 
-fst =  fc1.next();fc2.next();fc3.next();fc4.next();
+fst =  fc1.next();fc2.next();fc3.next();fc4.next();fc5.next();
 w.writerow(fst)
 
 
@@ -18,9 +19,10 @@ for row1 in fc1:
     row2 = fc2.next()
     row3 = fc3.next()
     row4 = fc4.next()
+    row5 = fc5.next()
     newLine = [row1[0]]
     for c in range(1, len(row1)):
-        newLine.append(str((float(row1[c]) + float(row2[c]) + float(row3[c]) + float(row4[c]))/4.0))
+        newLine.append(str((float(row1[c]) + float(row2[c]) + float(row3[c]) + float(row4[c]) + float(row5[c]))/5.0))
     w.writerow(newLine);
 fout.close()
 
