8a67d7f8b41d32701f4a71817e33729b768c6a6e
Oliver4242 <oliver.duerr@gmail.com>
2015-03-25 22:31:24 +0100

    Creation of the first simple network in lasgane

---
+++ python/TestingLasagne/SimpleNet.py
@@ -0,0 +1,42 @@
+__author__ = 'oli'
+from lasagne import layers
+from lasagne import nonlinearities
+from nolearn.lasagne import BatchIterator
+from lasagne import nonlinearities
+from nolearn.lasagne import NeuralNet
+
+from load_mnist import *
+
+X,y,PIXELS = load_data_2d(file='../../data/mnist.pkl.gz')
+
+
+net1 = NeuralNet(
+    # Geometrie of the network
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv2DLayer),
+        ('pool1', layers.MaxPool2DLayer),
+        ('conv2', layers.Conv2DLayer),
+        ('pool2', layers.MaxPool2DLayer),
+        ('hidden4', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    input_shape=(None, 1, PIXELS, PIXELS),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_ds=(2, 2),
+    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_ds=(2, 2),
+    hidden4_num_units=500,
+    output_num_units=10, output_nonlinearity=nonlinearities.softmax,
+
+    # learning rate parameters
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+    regression=False,
+    # We only train for 10 epochs
+    max_epochs=10,
+    verbose=1,
+
+    # Training test-set split
+    eval_size = 0.2
+    )
+
+net1.fit(X[0:1000,:,:,:],y[0:1000])
